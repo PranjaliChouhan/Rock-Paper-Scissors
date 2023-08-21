2671c928c2ea6c3208be62ae1b4d5b0679930cb9
PranjaliChouhan <pranjalichouhan4@gmail.com>
2023-08-22 00:56:53 +0530

    update

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,49 +1,94 @@
-import logo from './logo.svg';
+import "./App.css";
 import React, { useState } from 'react';
-import './App.css';
+import { Button, Grid, Typography, Container, Paper, Box } from '@mui/material';
+import paper from "./assets/paper.png";
+import rock from "./assets/rock.png";
+import scissor from "./assets/scissor.png";
 
-const choices = ['rock', 'paper', 'scissors'];
+const choices = [
+  { id: 'rock', image: rock },
+  { id: 'paper', image: paper },
+  { id: 'scissor', image: scissor },
+];
 
-function App() {
-  const [userChoice, setUserChoice] = useState(null);
+const App = () => {
+  const [playerChoice, setPlayerChoice] = useState(choices[0]); // Set an initial choice
   const [computerChoice, setComputerChoice] = useState(null);
-  const [result, setResult] = useState(null);
+  const [winner, setWinner] = useState(null);
+  const [playerScore, setPlayerScore] = useState(0);
+  const [computerScore, setComputerScore] = useState(0);
 
-  const playGame = (choice) => {
-    const computerChoice = choices[Math.floor(Math.random() * choices.length)];
-    setUserChoice(choice);
-    setComputerChoice(computerChoice);
+  const handleMoveSelection = (move) => {
+    const computerMove = choices[Math.floor(Math.random() * choices.length)];
+    setPlayerChoice(move);
+    setComputerChoice(computerMove);
 
-    if (choice === computerChoice) {
-      setResult('It\'s a tie!');
+    // Determine the winner
+    if (move.id === computerMove.id) {
+      setWinner('tie');
     } else if (
-      (choice === 'rock' && computerChoice === 'scissors') ||
-      (choice === 'paper' && computerChoice === 'rock') ||
-      (choice === 'scissors' && computerChoice === 'paper')
+      (move.id === 'rock' && computerMove.id === 'scissor') ||
+      (move.id === 'paper' && computerMove.id === 'rock') ||
+      (move.id === 'scissor' && computerMove.id === 'paper')
     ) {
-      setResult('You win!');
+      setWinner('player');
+      setPlayerScore((prevScore) => prevScore + 1);
     } else {
-      setResult('Computer wins!');
+      setWinner('computer');
+      setComputerScore((prevScore) => prevScore + 1);
     }
   };
 
   return (
-    <div className="App">
-      <h1>Rock Paper Scissors</h1>
-      <div>
-        <button onClick={() => playGame('rock')}>Rock</button>
-        <button onClick={() => playGame('paper')}>Paper</button>
-        <button onClick={() => playGame('scissors')}>Scissors</button>
-      </div>
-      {userChoice && computerChoice && result && (
-        <div>
-          <p>You chose {userChoice}.</p>
-          <p>Computer chose {computerChoice}.</p>
-          <p>{result}</p>
-        </div>
+    <Container maxWidth="sm">
+      <Typography variant="h4" align="center" gutterBottom>
+        Rock, Paper, Scissors
+      </Typography>
+      <Grid container spacing={2} justifyContent="center">
+        {choices.map((choice) => (
+          <Grid item key={choice.id}>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={() => handleMoveSelection(choice)}
+            >
+              <img
+                className='image'
+                src={choice.image}
+                alt={choice.id}
+                style={{ cursor: 'pointer' }}
+              />
+            </Button>
+          </Grid>
+        ))}
+      </Grid>
+     
+      {playerChoice && computerChoice && ( // Check if choices are not null
+        <Typography variant="h6" align="center" gutterBottom>
+          {`You chose ${playerChoice.id}. Computer chose ${computerChoice.id}.`}
+        </Typography>
       )}
-    </div>
+      {winner && (
+        <Typography variant="h5" align="center">
+          {winner === 'tie'
+            ? "It's a tie!"
+            : winner === 'player'
+            ? 'You win!'
+            : 'Computer wins!'}
+        </Typography>
+      )}
+       <Box mt={2} >
+        <Paper elevation={3}  className="score">
+          <Typography variant="h6" align="center" gutterBottom>
+            Score Card
+          </Typography>
+          <Typography variant="body1"className="score-text">
+            Player: {playerScore} &nbsp;&nbsp;&nbsp;&nbsp; Computer: {computerScore}
+          </Typography>
+        </Paper>
+      </Box>
+    </Container>
   );
-}
+};
 
 export default App;
